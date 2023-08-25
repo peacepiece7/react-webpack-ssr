4fcb3ca25b37b8510186cd78e24d8fb1102f8671
peacepiece7 <peacepiece7@naver.com>
2023-08-25 15:56:21 +0900

    fix : running HMR in production env

--- webpack.client.config.js
+++ webpack.client.config.js
@@ -16,40 +16,42 @@ const getEntryPoint = (target) => {
   return devMode ? [hotMiddlewareScript, './src/index.tsx'] : ['./src/index.tsx']
 }
 
-const getConfig = (target) => ({
-  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
-  name: target,
-  target,
-  entry: getEntryPoint(target),
-  output: {
-    path: path.resolve(__dirname, `dist/${target}`),
-    filename: '[name].js',
-    publicPath: '/web/',
-    libraryTarget: target === 'node' ? 'commonjs2' : undefined,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$|\.tsx?$/,
-        use: 'babel-loader',
-      },
-      {
-        test: /\.css?$/,
-        exclude: [],
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx'],
-  },
-  plugins:
-    target === 'web'
-      ? [new LoadablePlugin(), new MiniCssExtractPlugin(), new webpack.HotModuleReplacementPlugin()]
-      : [new LoadablePlugin(), new MiniCssExtractPlugin()],
+const getConfig = (target) => {
+  return {
+    mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+    name: target,
+    target,
+    entry: getEntryPoint(target),
+    output: {
+      path: path.resolve(__dirname, `dist/${target}`),
+      filename: '[name].js',
+      publicPath: '/web/',
+      libraryTarget: target === 'node' ? 'commonjs2' : undefined,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$|\.tsx?$/,
+          use: 'babel-loader',
+        },
+        {
+          test: /\.css?$/,
+          exclude: [],
+          use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    },
+    plugins:
+      target === 'web'
+        ? [new LoadablePlugin(), new MiniCssExtractPlugin(), new webpack.HotModuleReplacementPlugin()]
+        : [new LoadablePlugin(), new MiniCssExtractPlugin()],
 
-  // node환경에서 npm pacakge는 제외하지만, node_modules/@loadable/component는 제외하지 않음
-  externals: target === 'node' ? ['@loadable/component', webpackNodeExternals()] : undefined,
-})
+    // node환경에서 npm pacakge는 제외하지만, node_modules/@loadable/component는 제외하지 않음
+    externals: target === 'node' ? ['@loadable/component', webpackNodeExternals()] : undefined,
+  }
+}
 
 module.exports = [getConfig('web'), getConfig('node')]
